1cbca8c4aeaae5d6385c4bda14e001198fb21b21
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-05-28 15:41:17 +0300

    fdwv

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from "react"
-import Test from './Component/Test';
 
 function App() {
   const BASE_URL = process.env.REACT_APP_BASE_URL;
-  console.log(REACT_APP_BASE_URL);
+  console.log(BASE_URL);
   return (
     <div className="App">
-      <Test/>
+
     </div>
   );
 }
